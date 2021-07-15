ace2483f4214996c53fc37468dd9ae15829a55b5
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-07-15 17:01:48 -0400

    Load tasks automatically from object array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 import _ from 'lodash';
 import './style.css';
 
-const createDiv = () => document.createElement('div');
+const createElement = (name) => document.createElement(name);
+const createElementWithClass = (name, clazz) => {
+  const element = createElement(name);
+  element.className = clazz;
+
+  return element;
+};
+
+const createDivWithClass = (clazz) => createElementWithClass('div', clazz);
+const getLabelInnerHTML = (task, checkboxId) => {
+  let checkboxHTML = `<input type="checkbox" name="${checkboxId}" id="${checkboxId}">`;
+  if(task.completed) {
+    checkboxHTML = `<input type="checkbox" name="${checkboxId}" id="${checkboxId}" checked="checked">`;
+  }
+
+  return `${checkboxHTML} <span class="checkmark"></span>`;
+};
+const createDescriptionDiv = (task) => {
+  let descriptionDivClass = 'to-do-list-task--description';
+  if(task.completed) {
+    descriptionDivClass += ' strikethrough';
+  }
+
+  const descriptionDiv = createDivWithClass(descriptionDivClass);
+  descriptionDiv.textContent = task.description;
+
+  return descriptionDiv;
+}
+
+function buildTaskLeftElement(task) {
+  const taskLeftElement = createDivWithClass('todo-list-task-left');
+
+  /* Build checkbox */
+  const checkboxId = `check-task-${task.index}`;
+
+  const labelElement = createElementWithClass('label', 'container');
+  labelElement.setAttribute('for', checkboxId);
+  labelElement.innerHTML = getLabelInnerHTML(task, checkboxId);
+
+  /* Build description div */
+  const descriptionDiv = createDescriptionDiv(task);
+  
+  taskLeftElement.appendChild(labelElement);
+  taskLeftElement.appendChild(descriptionDiv);
+  return taskLeftElement;
+}
+
+function buildTaskRightElement() {
+  const taskRightElement = createDivWithClass('todo-list-task-right');
+  taskRightElement.innerHTML = '<i class="fa fa-ellipsis-v" aria-hidden="true"></i>';
+
+  return taskRightElement;
+}
 
 function buildTaskElement(task) {
-  const taskElement = createDiv();
-  taskElement.className = 'todo-list-task';
-  taskElement.textContent = task.description;
+  const taskElement = createDivWithClass('todo-list-task');
+  taskElement.appendChild(buildTaskLeftElement(task))
+  taskElement.appendChild(buildTaskRightElement());
 
   return taskElement;
 }
